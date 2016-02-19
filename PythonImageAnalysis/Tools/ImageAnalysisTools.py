184650aaa184e8ccb34723a859298004479662c2
DarioMiroli <s1033855@sms.ed.ac.uk>
2016-02-19 10:23:21 +0000

    Added more doc strings

--- PythonImageAnalysis/Tools/ImageAnalysisTools.py
+++ PythonImageAnalysis/Tools/ImageAnalysisTools.py
@@ -1,4 +1,4 @@
-"""Functions used in Image analysis process"""
+"""Mix of functions used for image processing and data extraction."""
 import matplotlib.pyplot as plt
 import numpy as np
 from skimage.filters import *
@@ -16,7 +16,10 @@ from matplotlib.ticker import MultipleLocator
 import os
 
 def Setup(InputFolder, OutputFolder):
-    '''Setup Function'''
+    '''
+    Creates output folder if it doesnt exist and get input file named from input
+    file.
+    '''
     try:
         os.stat(OutputFolder)
     except:
@@ -25,42 +28,72 @@ def Setup(InputFolder, OutputFolder):
     return filenames
 
 def Open(pathname):
+    '''
+    Returns numpy array of image at pathname.
+    '''
     Image = plt.imread(pathname)
     Image_Array = np.array(Image, dtype = np.uint16)
     return(Image_Array)
 
 def Blurr(Image,sigma):
+    '''
+    Perofrms a gaussian blurr on Image with standard deviation sigma.
+    '''
     Blurred_Image = gaussian_filter(Image,sigma=sigma)
     return(Blurred_Image)
 
 def Threshold(Image,blockSize):
+    '''
+    Performs an adaptive threshold on Image. Threshold value is obtained by
+    averaging pixels in each small reigon the size of which is given by
+    blocksize.
+    '''
     Threshold_Image = threshold_adaptive(Image, blockSize)
     return(Threshold_Image)
 
 def GlobalThreshold(Image):
+    '''
+    Simple threshold of image using single threshold value over whole image.
+    '''
     thresh = threshold_otsu(Image)
     Image = Image > thresh
     return(Image)
 
 def Erode(Image,numberoftimes):
+    '''
+    Performs binary erosion of image a required number of times.
+    '''
     for i in range(numberoftimes):
         Image = binary_erosion(Image)
     return(Image)
 
 def Dilate(Image,numberoftimes):
+    '''
+    Performs binary dilation on Image a required number of times.
+    '''
     for i in range(numberoftimes):
         Image = binary_dilation(Image)
     return(Image)
 
 def DistanceTransform(Image):
+    '''
+    Takes distance transform of image.
+    '''
     return(ndi.distance_transform_edt(Image))
 
 def Label(Image):
+    '''
+    Labels each connected reigon in Image with a different number
+    '''
     Labelled_Image = measure.label(Image, background=0)
     Labelled_Image,a,b = segmentation.relabel_sequential(Labelled_Image, offset=1)
     return(Labelled_Image)
 
 def WaterShed(Image):
+    '''
+    Performs water shed transform on Image. Useful for segmentation of connected
+    reigons.
+    '''
     Distance_Transformed_Image = DistanceTransform(Image)
     Local_Maxima = peak_local_max(Distance_Transformed_Image, indices=False, footprint=np.ones((5, 5)),labels=Image)
     markers = measure.label(Local_Maxima)
@@ -68,9 +101,16 @@ def WaterShed(Image):
     return(Watershed_Image)
 
 def Skeletonize(Image):
+    '''
+    Reduces Image to a skeleton.
+    '''
     return(skeletonize(Image))
 
 def SiveArea(Image,smallest=0,largest=1E9):
+    '''
+    Removes connected reigons smller than smallest and larger than largest in
+    Image.
+    '''
     Image = np.copy(Image)
     BadReigons = []
     for reigon in measure.regionprops(Image):
@@ -82,10 +122,16 @@ def SiveArea(Image,smallest=0,largest=1E9):
     return(Image)
 
 def Fill(Image):
+    '''
+    Fills in whole in connected reigons in Image.
+    '''
     Filled = ndi.binary_fill_holes(Image)
     return(Filled)
 
 def ConstructOverlay(Image1,Image2):
+    '''
+    Takes 2 images and overlays them for disply.
+    '''
     p2, p98 = np.percentile(Image1, (0, 100))
     stretched_Image = exposure.rescale_intensity(Image1, in_range=(p2, p98))
     zeros = np.zeros((np.size(Image1,0),np.size(Image1,0)), dtype = np.uint16)
@@ -93,10 +139,16 @@ def ConstructOverlay(Image1,Image2):
     return overlay
 
 def Save(Image,pathname):
+    '''
+    Saves image to pathname as png.
+    '''
     io.imsave(pathname, Image)
 
 
 def Compare(ImageArray,ColorBarArray=None, TitleArray=None):
+    '''
+    Places images side by side for comparsion.
+    '''
     if TitleArray == None:
         TitleArray = ["A","B","C","D","E","F","G","H","I","J","K","L"]
     if ColorBarArray == None:
@@ -124,12 +176,21 @@ def Compare(ImageArray,ColorBarArray=None, TitleArray=None):
     #plt.savefig(FileName + ".png")
 
 def Show():
+    '''
+    Shows image at run time.
+    '''
     plt.show()
 
 def SavePlot(filename):
+    '''
+    Saves plot to pathname as png.
+    '''
     plt.savefig(filename + ".png")
 
 def FolderCompare(FolderName):
+    '''
+    Compares all images in a folder on screen.
+    '''
     filenames = os.listdir(FolderName)
     Images = []
     for file in filenames:
