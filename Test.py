3f6b318c38ae307d5d2c40ca5f0135bcb279cb6c
DarioMiroli <s1033855@sms.ed.ac.uk>
2017-01-18 11:55:00 +0000

    Finalized packaging of Image toolbox

--- Test.py
+++ Test.py
@@ -2,4 +2,7 @@
 at the correct place in the file hierachy such that pyzota_image_toolbox can be
 imported as a standard package without needing to make changes to the path."""
 import pyzota_image_toolbox
-from pyzota_image_toolbox import imageTools as it
+from pyzota_image_toolbox import imageTools as IT
+import numpy as np
+
+print("Success")
