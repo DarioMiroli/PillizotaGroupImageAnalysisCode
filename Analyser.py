a97882254a7fd2de2cd4ec3ae6351bfe53579178
DarioMiroli <s1033855@sms.ed.ac.uk>
2018-01-17 15:00:05 +0000

    Fixed errors in grouping by condition

--- Analyser.py
+++ Analyser.py
@@ -4,30 +4,37 @@ import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 import copy
+
+def getCondition(fName):
+    array = fName.split(".")[0].split("_")[0:2]
+    return array[0]+array[1]
+
+
 dataFolder = "./Analysis/AllData/"
 fileNames = sorted(IT.GetFileNamesFromFolder(dataFolder))
 fileNamesPlusPath = sorted(IT.GetFileNamesFromFolder(dataFolder,False))
 areaCutOff = 10
-array = [[] for i in range(len(fileNames))]
+
+
+numberOfFiles = len(fileNames)
+print("Number of files = {}".format(numberOfFiles))
+conditions =sorted(list(set([ getCondition(i) for i in fileNames ])))
+print("Conditions={}".format(conditions))
+
+
+array = [[] for i in range(len(conditions))]
+#Conditions grouped together
 dataDic = {"Names":copy.deepcopy(array),"Areas":copy.deepcopy(array),
         "Widths":copy.deepcopy(array),"Lengths":copy.deepcopy(array)}
+
+array = [[] for i in range(numberOfFiles)]
+#Each slide individually
 dataDic2 = {"Names":copy.deepcopy(array),"Areas":copy.deepcopy(array),
         "Widths":copy.deepcopy(array),"Lengths":copy.deepcopy(array)}
 
-conditions = [[],[]]
+
+
 for n,fName in enumerate(fileNamesPlusPath):
-    m = n
-    condition = fileNames[n].split(".")[0].split("_")[0:2]
-    condition = condition[0]+condition[1]
-    if condition in conditions[0]:
-        N = conditions[1][np.where(np.asarray(condition)==conditions[0])[0][0]]
-        dataDic2["Names"][m].append(fileNames[m])
-    else:
-        conditions[0].append(condition)
-        conditions[1].append(n)
-        dataDic["Names"][n].append(fileNames[n])
-        dataDic2["Names"][m].append(fileNames[m])
-        N=n
     f = open(fName,'r')
     endOfFile = False
     while not endOfFile:
@@ -38,21 +45,24 @@ for n,fName in enumerate(fileNamesPlusPath):
                 mask = IT.ClearBorders(mask)
                 area = IT.GetArea(mask)
                 if area > areaCutOff:
-                    dataDic["Areas"][N].append(area)
-                    dataDic2["Areas"][m].append(area)
-                    try:
-                        length,width = IT.GetLengthAndWidth(mask)
-                    except:
-                        plt.show()
-
-                    dataDic["Lengths"][N].append(length)
-                    dataDic2["Lengths"][m].append(length)
-                    dataDic["Widths"][N].append(width)
-                    dataDic2["Widths"][m].append(width)
-
+                    length,width = IT.GetLengthAndWidth(mask)
+                    condition = getCondition(fileNames[n])
+                    index = conditions.index(condition)
+                    dataDic["Areas"][index].append(area)
+                    dataDic["Lengths"][index].append(length)
+                    dataDic["Widths"][index].append(width)
+                    dataDic2["Areas"][n].append(area)
+                    dataDic2["Lengths"][n].append(length)
+                    dataDic2["Widths"][n].append(width)
         except EOFError:
             print("End of File")
             endOfFile = True
+
+#****************************************** PLOTTING ***************************
+
+
+
+#****************************************** PLOTTING ***************************
 
 
 plt.ioff()
@@ -61,8 +71,8 @@ plt.clf()
 Props = ["Areas","Lengths","Widths"]
 histBins = [np.linspace(0,2000,50),np.linspace(0,100,50),np.linspace(0,25,50)]
 for z,prop in enumerate(Props):
-    for n in range(len(conditions[0])):
-        plt.hist(dataDic[prop][n],bins=histBins[z],label=conditions[0][n],alpha=0.5)
+    for n in range(len(conditions)):
+        plt.hist(dataDic[prop][n],bins=histBins[z],label=conditions[n],alpha=0.5)
     plt.legend()
     plt.title(prop)
     plt.xlabel(prop)
@@ -75,12 +85,12 @@ for prop in Props:
     data = []
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    for n in range(len(conditions[0])):
+    for n in range(len(conditions)):
         data.append(np.asarray(dataDic[prop][n])*0.05)
         ax.annotate('n={}'.format(len(data[n])), xy=(n+1, np.median(data[n])),
                 xytext=(n+1, np.median(data[n])), ha='center')
     ax.boxplot(data)
-    ax.set_xticklabels(conditions[0])
+    ax.set_xticklabels(conditions)
     plt.title(prop)
     ax.grid(linestyle='--', linewidth=1,axis="y")
     plt.savefig("./Analysis/Graphs/"+prop)
